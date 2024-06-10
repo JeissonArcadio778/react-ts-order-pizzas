3cdaa45ba6134bbc9e2c56ed997cdf5750afedb0
jeissonarcadio778 <jeissonbeltranporras@gmail.com>
2024-06-09 21:49:22 -0400

    chore: update package name and website description

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,3 @@
-/**
- * Archivo principal de la aplicación.
- * 
- * Este archivo renderiza el componente principal de la aplicación en el elemento con el id 'root'.
- * También llama a la función reportWebVitals para medir el rendimiento de la aplicación.
- */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
