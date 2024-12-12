03c0921c816c36b70d222f82e3b19b1a1cbdd576
jeissonarcadio778 <jeissonbeltranporras@gmail.com>
2024-12-11 21:48:39 -0500

    feat: conditionally render authentication buttons on LandingPage based on user authentication status

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './styles/LandingPageStyles.css';
+import { AuthProvider, useAuth } from '../context/AuthContext';
 
 export const LandingPage: React.FC = () => {
+  const { isAuthenticated } = useAuth();
   return (
     <div className="form-container">
       <h1>BIENVENIDOS A MANDIRICAS</h1>
       <div className="image-placeholder">
         <img src={require('./assets/images/mandiricas-logo.png')} alt="Welcome to Mandiricas" />
       </div>
-      <div className="button-group">
-        <Link to="/create-account" className="btn">Crear Cuenta</Link>
-        <Link to="/login" className="btn">Iniciar Sesión</Link>
-      </div>
+
+      {!isAuthenticated && (
+        <div className="button-group">
+          <Link to="/create-account" className="btn">Crear Cuenta</Link>
+          <Link to="/login" className="btn">Iniciar Sesión</Link>
+        </div>
+      )}
     </div>
   );
 };
